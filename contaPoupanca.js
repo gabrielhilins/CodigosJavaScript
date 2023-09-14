4717c4b082472a9751b5938f2a5cd183d93a53d8
Gabriel Henrique Lins <130785312+gabrielhilins@users.noreply.github.com>
2023-09-14 15:42:55 -0300

    Delete contaPoupanca.js

--- contaPoupanca.js
+++
@@ -1,8 +0,0 @@
-import contaBancaria from './contaBancaria.js';
-
-class contaPoupanca extends contaBancaria {
-    constructor(agencia, numero, tipo = 'conta poupanca', saldo) {
-        super(agencia, numero, tipo, saldo);
-    }
-}
-export default contaPoupanca;
